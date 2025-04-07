82d93ade8b65c6cf2ddf29472e9e5c1d0b9c6e1c
lag <qmariboc@gmail.com>
2025-04-07 17:16:36 -0500

    Update Devil_may_cry.js

--- Devil_may_cry.js
+++ Devil_may_cry.js
@@ -6,14 +6,14 @@ const pauseButton = document.getElementById("pause-button");
 
 // Enlaces y nombres de los videos
 const videos = [
-  { name: "EPISODOIO 01", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x01%20-%20Infierno.mp4" },
-  { name: "EPISOIDIO 02", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x02%20-%20Nuestra%20Se%C3%B1ora%20de%20los%20Dolores.mp4" },
-  { name: "EPISOIDIO 03", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x03%20-%20El%20camino%20profundo%20y%20salvaje.mp4" },
-  { name: "EPISOIDIO 04", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x04%20-%20Abandona%20toda%20esperanza.mp4" },
-  { name: "EPISOIDIO 05", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x05%20-%20El%20descenso.mp4" },
-  { name: "EPISOIDIO 06", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x06%20-%20El%20primer%20c%C3%ADrculo.mp4" },
-  { name: "EPISOIDIO 07", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x07%20-%20Ante%20las%20puertas%20del%20para%C3%ADso.mp4" },
-  { name: "EPISOIDIO 08", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x08%20-%20Un%20r%C3%ADo%20de%20sangre%20hirviente.mp4" }
+  { name: "EPISODIO 01", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x01%20-%20Infierno.mp4" },
+  { name: "EPISODIO 02", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x02%20-%20Nuestra%20Se%C3%B1ora%20de%20los%20Dolores.mp4" },
+  { name: "EPISODIO 03", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x03%20-%20El%20camino%20profundo%20y%20salvaje.mp4" },
+  { name: "EPISODIO 04", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x04%20-%20Abandona%20toda%20esperanza.mp4" },
+  { name: "EPISODIO 05", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x05%20-%20El%20descenso.mp4" },
+  { name: "EPISODIO 06", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x06%20-%20El%20primer%20c%C3%ADrculo.mp4" },
+  { name: "EPISODIO 07", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x07%20-%20Ante%20las%20puertas%20del%20para%C3%ADso.mp4" },
+  { name: "EPISODIO 08", url: "https://pub-192f85bc3f094c9d8efa6b544fa8fba4.r2.dev/Devil%20May%20Cry%202025/S01/Devil%20May%20Cry%20-%201x08%20-%20Un%20r%C3%ADo%20de%20sangre%20hirviente.mp4" }
 ];
 
 let currentIndex = 0;
